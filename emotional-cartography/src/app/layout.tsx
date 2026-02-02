aedd911f41a2c8e15702dd5aa46b1d9e1cd10575
Rachael Chew <rachaelchewyuen@gmail.com>
2026-02-02 20:39:54 +0800

    add layout

--- emotional-cartography/src/app/layout.tsx
+++ emotional-cartography/src/app/layout.tsx
@@ -28,8 +28,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-[#0a0a0f] text-white antialiased">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-[#0a0a0f] text-white antialiased" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
